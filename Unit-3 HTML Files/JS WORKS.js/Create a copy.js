a207ec01f3136dc7d7487d30bef567b9da1d6634
rajachoudhary <rajachoudhary341@gmail.com>
2021-09-16 12:07:49 +0530

    bootstap Assignment

--- Unit-3 HTML Files/JS WORKS.js/Create a copy.js
+++ Unit-3 HTML Files/JS WORKS.js/Create a copy.js
@@ -5,12 +5,14 @@ function array(arr){
     //pushing a value in arr1
     arr1.push(9)
     console.log(arr1)
-    console.log(arr)
+    
 
 }
 // creating a array 
 var arr = [1,2,3,4,5,6,7,8,]
 // passing an arr in function array() as an argument
+
 var arry = array(arr);
+console.log(arr)
 
 
