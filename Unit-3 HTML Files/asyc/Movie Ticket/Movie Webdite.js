3a77bb5985ba665496d441a69a17a2ee25ea9b3c
rajachoudhary <rajachoudhary341@gmail.com>
2021-09-25 21:52:49 +0530

    vaccine assignment

--- Unit-3 HTML Files/asyc/Movie Ticket/Movie Webdite.js
+++ Unit-3 HTML Files/asyc/Movie Ticket/Movie Webdite.js
@@ -1,13 +1,21 @@
 
 
-var container = document.getElementById("container")
 
 let arr = ["https://www.filmibeat.com/imgm/416x335x100/2019/03/the-curse-of-la-llorona_155316627520.jpg","https://www.filmibeat.com/imgm/416x335x100/2017/02/kong-skull-island_1488280025250.jpg"]
-for (var i = 0; i <arr.length; i++){
 
+
+var container = document.getElementById("container")
+
+function display(arr) {
+
+    for (var i = 0; i < arr.length; i++){
         var img = document.createElement("img")
         img.src = arr[i]
-    }
-    setInterval(function() {
+        console.log(img.src)
         container.appendChild(img)
-    },2000)
+    }
+}
+setTimeout("display(arr)",1000)
+
+
+
