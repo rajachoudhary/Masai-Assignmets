b7bad9405fa695548d75403afd69c88aae4018bf
rajachoudhary <rajachoudhary341@gmail.com>
2021-09-27 11:53:00 +0530

    assignments

--- Unit-3 HTML Files/asyc/Movie Ticket/Movie Webdite.js
+++ Unit-3 HTML Files/asyc/Movie Ticket/Movie Webdite.js
@@ -1,21 +1,33 @@
 
 
 
-let arr = ["https://www.filmibeat.com/imgm/416x335x100/2019/03/the-curse-of-la-llorona_155316627520.jpg","https://www.filmibeat.com/imgm/416x335x100/2017/02/kong-skull-island_1488280025250.jpg"]
-
+let arr = ["https://images.cinemaexpress.com/uploads/user/imagelibrary/2020/5/1/original/3_Idiots.PNG"
+            ,"https://wallpaperaccess.com/full/1923020.jpg"
+            ,"https://sofy.tv/blog/wp-content/uploads/2017/12/upcoming-hollywood-movies-you-shouldnt-miss-1400x653-1505901970_1100x513.jpg"
+            ,"https://media.easemytrip.com/media/Blog/India/636929292840157797/636929292840157797k5vsjs.png"
+            ,"https://img.mensxp.com/media/content/2019/Aug/before-amp-after-hollywood-movie-scenes-with-special-effects-that-show-the-power-of-cgi-1400x653-1566306171.jpg"
+            ,"https://www.bollywoodhungama.com/wp-content/uploads/2020/04/Hollywood-movies-have-amassed-only-Rs.-50-crores-at-the-India-box-office-in-the-first-quarter-of-2020.jpg"
+            ,"https://i.pinimg.com/550x/3a/6a/2b/3a6a2bd9a83283cd6ef230ab6a1df4dc.jpg"]
+var i = 0 
 
 var container = document.getElementById("container")
 
-function display(arr) {
-
-    for (var i = 0; i < arr.length; i++){
-        var img = document.createElement("img")
-        img.src = arr[i]
-        console.log(img.src)
-        container.appendChild(img)
+var img = document.createElement("img")
+// img.className = "img"
+function imageChnage(){
+    img.src = arr[i]
+    if(i<arr.length-1){
+        i++
+    } else{
+        i=0
     }
+    setTimeout(imageChnage,2000)
 }
-setTimeout("display(arr)",1000)
+container.appendChild(img)
+
+window.addEventListener("load", imageChnage)
+
+
 
 
 
