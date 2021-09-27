82e123ef8f5bf356773f767f30f2f7bd0b443620
rajachoudhary <rajachoudhary341@gmail.com>
2021-09-27 17:58:32 +0530

    Tatkal Ticket Assignment

---
+++ Unit-3 HTML Files/promises/Tatkal Tciket.js
@@ -0,0 +1,86 @@
+const trainA = ["a","b","c","d","e","f","g","h","i"];
+const trainB = ["j","k","l","m","n","o","p","q","r"];
+const trainC  = ["s","t","u","v","w","X","Y","z"];
+
+async function checkAvailability(){
+
+    let promise = await new Promise(function(resolve,reject){
+        const id = setInterval (function(){
+            if(trainA.length < 5){
+                resolve (`Train A is available`)
+                clearInterval(id)
+            }
+            else if(trainB.length < 5){
+                resolve (`Train b is available`)
+                clearInterval(id)
+            }
+             else if (trainC.length < 5){
+                resolve (`Train c is available`)
+                clearInterval(id)
+            }
+        },1000)
+        
+    })
+    return promise;
+}
+
+function cancelTicket(){
+
+    let speeds = [
+        Math.floor(Math.random() * 5),
+        Math.floor(Math.random() * 10),
+        Math.floor(Math.random() * 8)
+    ];
+
+    const ids = ["A","B","C"];
+    [trainA, trainB, trainC].forEach(function(train,index){
+
+        let trainA = document.getElementById("trainA")
+        let trainB = document.getElementById("trainB")
+        let trainC = document.getElementById("trainC")
+        
+        if(index === 0){
+           trainA.textContent = train.length
+        }  
+        if (index === 1){
+            trainB.textContent= train.length
+        }  
+        if (index){
+            trainC.textContent = train.length
+        }
+
+        setTimeout(function(){
+            train.pop();
+            visualize(train,ids[index])
+            console.log(ids[index])
+        },1000*speeds[index])
+    })
+}
+
+function visualize(train,id){
+   
+    const qContainer = document.getElementById(id);
+    qContainer.innerHTML = ""
+    const ul = document.createElement("ul")
+    train.forEach(function(name){
+        
+        const el = document.createElement("li")
+        el.textContent = name
+        ul.append(el)
+    })
+    qContainer.appendChild(ul)
+}
+
+visualize(trainA,"A")
+visualize(trainB,"B")
+visualize(trainC,"C")
+
+var id = setInterval(cancelTicket,1000)
+
+checkAvailability()
+.then(function(resolve){
+    alert(resolve)
+})
+.catch(function(err){
+    console.log(err)
+})
