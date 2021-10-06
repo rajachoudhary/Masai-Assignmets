af0b785c2412a3b6765e0f07b2d9e6fa486f5af3
rajachoudhary <rajachoudhary341@gmail.com>
2021-10-06 13:57:12 +0530

    recursion

--- DSA-Unit-3/Recursion/Recursive Bubble sort.js
+++ DSA-Unit-3/Recursion/Recursive Bubble sort.js
@@ -1,13 +1,16 @@
 function sorting(N,arr){
-   
-    for(var i=0;i<arr.length-1;i++) {
-        
-        if(arr[i]>arr[i+1]){
-            var temp = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = temp;
-        } else {
-            arr[i] === arr[i]
+    if(N==1){
+        return 
+    } else {
+
+        for(var i=0;i<N-1;i++) {
+            
+            if(arr[i]>arr[i+1]){
+                var temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            } 
+            sorting(N-1,arr)
         }
     }
 }
@@ -17,8 +20,8 @@ function runProgram(input){
     input = input.split("\n")
     var N = Number(input[0])
     var arr = input[1].split(" ").map(Number)
-    var output = sorting (N,arr)
-    console.log(output)
+    sorting (N,arr)
+    console.log(arr.join(" "))
 }
 runProgram(`5
 3 5 0 9 8`)
