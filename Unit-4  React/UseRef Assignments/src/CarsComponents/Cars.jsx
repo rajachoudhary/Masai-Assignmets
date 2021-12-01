596b71c297ec4a40a79bfeebd4e54e1d1bb8be64
rajachoudhary <rajachoudhary341@gmail.com>
2021-12-01 20:47:15 +0530

    get Cars

--- Unit-4  React/UseRef Assignments/src/CarsComponents/Cars.jsx
+++ Unit-4  React/UseRef Assignments/src/CarsComponents/Cars.jsx
@@ -1,1 +1,46 @@
 import axios from "axios";
+import {useState,useEffect} from "react"
+// import React from "react"
+
+const getCars = () => {
+    const config = {
+      url: "http://localhost:3000/cars",
+      method: "get"
+    };
+    return axios(config);
+  };
+
+const Cars = ()=>{
+    const [isLoading, setIsLoading] = useState(true);
+    const [cars,setCars]=useState([])
+
+    useEffect(() => {  
+        return handleGetCars();
+    }, [])
+
+    const handleGetCars = () => {
+        return getCars()
+          .then((res) => {
+            setCars(res.data);
+            setIsLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    };
+
+    return (
+        <div>
+          <div>
+            {cars.map((item) => (
+              <div key={item.id}>
+                <div>{item.name}</div>
+                <div>{item.type}</div>
+              </div>
+            ))}
+          </div>
+          
+        </div>
+      );
+}
+export default Cars
