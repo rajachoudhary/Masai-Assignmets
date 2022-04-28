76b22e8e6b07db937246d4cd0d4191b55cea0376
rajachoudhary <rajachoudhary.com>
2022-04-28 06:03:23 +0530

    pushing all files

--- React revision/redux-calculator/src/redux/store.js
+++ React revision/redux-calculator/src/redux/store.js
@@ -1,3 +1,3 @@
 import { reducer } from "./reducer";
 import {createStore} from "redux"
-export const store = createStore(reducer)
+export const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
